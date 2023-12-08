6bbd643e655c28cc5de827df0fb0db4711c7b3eb
NakamotoNomad <nakamotonomad@outlook.com>
2023-12-08 18:00:16 +0700

    Created 4 NFTs and deployed them to Goerli
    Contract on Goerli: 0x82c5Dc2f796195dA8dBdeB104B7f1c203b99FF24

--- ipfs/upload.js
+++ ipfs/upload.js
@@ -1,34 +1,104 @@
 async function run() {
     const { create } = await import('ipfs-http-client');
     const ipfs = await create();
-    
-    // we added three attributes, add as many as you want!
-    const metadata = {
+
+    const metadata1 = {
         path: '/',
         content: JSON.stringify({
-            name: "My First NFT",
+            name: "Balance",
             attributes: [
             {
-                "trait_type": "Peace",
-                "value": "10" 
+                "trait_type": "Fortune",
+                "value": "33"
             },
             {
-                "trait_type": "Love",
+                "trait_type": "Treasure",
+                "value": "33"
+            },
+            {
+                "trait_type": "Wealth",
+                "value": "33"
+            }
+            ],
+            image: "https://ipfs.io/ipfs/QmfU82Q1DMH3oXWUu6HMLjd14QDm2L6vj8AL4cCbGDpo1v?filename=1.png",
+            description: "Jack of all trades, master of none, though often better than a master of one."
+        })
+    };
+    const metadata2 = {
+        path: '/',
+        content: JSON.stringify({
+            name: "My Precious",
+            attributes: [
+            {
+                "trait_type": "Fortune",
+                "value": "0"
+            },
+            {
+                "trait_type": "Treasure",
                 "value": "100"
             },
             {
-                "trait_type": "Web3",
-                "value": "1000"
+                "trait_type": "Wealth",
+                "value": "0"
             }
             ],
-            // update the IPFS CID to be your image CID
-            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
-            description: "So much PLW3!"
+            image: "https://ipfs.io/ipfs/QmY6jJLD1zcmdCwkCBnJDvRmchcRHbkVJ9aGGsNXU8CAvb?filename=2.png",
+            description: "How to get rich in 3 easy steps.\nStep 1: Don't blindly trust the description within NFTs claiming they'll make you rich.\nStep 2: ???\nStep 3: Profit!"
+        })
+    };
+    const metadata3 = {
+        path: '/',
+        content: JSON.stringify({
+            name: "Luck isn't finite",
+            attributes: [
+            {
+                "trait_type": "Fortune",
+                "value": "100"
+            },
+            {
+                "trait_type": "Treasure",
+                "value": "0"
+            },
+            {
+                "trait_type": "Wealth",
+                "value": "0"
+            }
+            ],
+            image: "https://ipfs.io/ipfs/QmaJ6Rb8D8uvRHehJdD7aqnDs1bnBzqx4tk4fpLWoUsd4E?filename=3.png",
+            description: "Unfortunately for you, this is a 1 of 1 and I ain't selling."
+        })
+    };
+    const metadata4 = {
+        path: '/',
+        content: JSON.stringify({
+            name: "Make it rain",
+            attributes: [
+            {
+                "trait_type": "Fortune",
+                "value": "0"
+            },
+            {
+                "trait_type": "Treasure",
+                "value": "0"
+            },
+            {
+                "trait_type": "Wealth",
+                "value": "100"
+            }
+            ],
+            image: "https://ipfs.io/ipfs/Qmd1b4DyMab75ufwRbZnKCXyuQkjopsYSt28WRPLiGTtfQ?filename=4.png",
+            description: "If money doesn't grow on trees, what's the paper it's printed on made of?"
         })
     };
 
-    const result = await ipfs.add(metadata);
-    console.log(result);
+    const result1 = await ipfs.add(metadata1);
+    console.log(result1);
+    const result2 = await ipfs.add(metadata2);
+    console.log(result2);
+    const result3 = await ipfs.add(metadata3);
+    console.log(result3);
+    const result4 = await ipfs.add(metadata4);
+    console.log(result4);
 
     process.exit(0);
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,5 @@
 async function main() {
-  // if you changed the name of the contract, be sure to update this here!
-  const MyToken = await hre.ethers.getContractFactory("MyToken");
+  const MyToken = await hre.ethers.getContractFactory("FortuneTreasureWealth");
 
   const nft = await MyToken.deploy();
 
@@ -11,7 +10,10 @@ async function main() {
   // mint one to yourself!
   const signer0 = await ethers.provider.getSigner(0);
   // update the IPFS CID to be your metadata CID
-  await nft.safeMint(await signer0.getAddress(), "ipfs://QmbCwoJB7g9b86BfEahsjzyzzNx3WEu7Jc1opXWgeTXFgb");
+  await nft.safeMint(await signer0.getAddress(), "ipfs://QmWS38j33NdyPQPmB5azWGvb7gM9eg6GzfZpGT3yhp9wod");
+  await nft.safeMint("0x4fcB1A896d53aa0F4883c760BB4b8f26af525416", "ipfs://QmYrAD95xvfaxv8gidYZvd23xwPDBaSJkSPyBvVTStywTR");
+  await nft.safeMint("0x4fcB1A896d53aa0F4883c760BB4b8f26af525416", "ipfs://QmZSHfuBV9Cvb5Cv9QvnDJY1jUvjPKowtk4Gx7gcTu624a");
+  await nft.safeMint("0x5F77940BA41CE6D78fE75074C7AB74A5D60f7EB2", "ipfs://QmTNAvgfUhxzg6yG2nrNNytbcGnQXrkt1Ft5LJ6ZtbKZJT");
 
   console.log("NFT Minted!");
 }
